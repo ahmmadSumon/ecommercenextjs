70050babc90d95ed6169644e6687f8291a2860f5
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-12-09 22:54:56 +0600

    qss

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -154,7 +154,7 @@ const Cart = () => {
               </button>
               </Link>
               <p className='text-center'>oR</p>
-              <Link href="/">
+              <Link href="/allproducts">
               <button
                 type="button"
                 className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800"
